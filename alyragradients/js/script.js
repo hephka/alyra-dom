b14fc968fcfd21a367321e42f4a8d525a42abe7a
Franck Thevenin <franckthevenin95@gmail.com>
2020-07-24 17:43:30 +0200

    alyragradients - btn non resolu

--- alyragradients/js/script.js
+++ alyragradients/js/script.js
@@ -1,15 +1,18 @@
-"use strict"
+"use strict";
 
 /* 
 le script gradients.js est chargé avant script.js
 la variable gradients est disponible - vous pouvez le voir dans la console
 */
-console.log(gradients)
+console.log(gradients);
 
 /* dans le paragraphe #intro afficher la phrase 
 Voici une collection de ... dégradés prêts à utiliser dans vos feuilles de styles
 
 ou ... correspond au nombre de dégradés */
+console.log(gradients.length);
+const pSumEl = document.getElementById("intro");
+pSumEl.textContent = `Voici une collection de ${gradients.length} dégradés prêts à utiliser dans vos feuilles de styless`;
 
 /* header : 
 ajouter la propriété backgroundImage au style de header
@@ -18,3 +21,19 @@ ou ... correspondent aux couleurs start et end d'un des objet de l'array gradien
 
 Le bouton #header-button permet de regénérer le header (=changer le dégradé)
 */
+
+function generateBgImg() {
+  const start = Math.floor(Math.random() * gradients.length);
+  const end = Math.floor(Math.random() * gradients.length);
+  const colorGradient = (document.getElementById(
+    "header"
+  ).style.backgroundImage = `linear-gradient(90deg, ${start}, ${end})`);
+  console.log(colorGradient);
+}
+
+const myButton = document.querySelector("header-button");
+myButton.addEventListener("click", () => {
+  return colorGradient;
+});
+
+generateBgImg();
